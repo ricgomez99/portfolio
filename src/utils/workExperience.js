57b7de234eb7bfa09fa72ba196da87565b2ed676
Andres <argomez.restrepo@gmail.com>
2025-02-03 10:56:16 -0500

    added experience section

---
+++ src/utils/workExperience.js
@@ -0,0 +1,38 @@
+export const workExperience = [
+  {
+    id: 1,
+    company: "Wibbux",
+    title: "FullStack Developer",
+    date: "May 2024 - Present",
+    description:
+      "Developed a scalable NFC-enabled web app with ReactJS, NestJS, and AWS, boosting productivity by 25% and improving API connectivity by 10% through backend engineering.",
+    tech: [
+      "React",
+      "NestJS",
+      "TypeScript",
+      "JavaScript",
+      "MongoDB",
+      "Mongoose",
+      "Kafka",
+      "TailwindCSS",
+    ],
+  },
+  {
+    id: 2,
+    company: "On the fuze",
+    title: "Frontend Developer",
+    date: "Mar 2023 - Sep 2023",
+    description:
+      "Built and optimized 30+ web projects with ReactJS and modular CSS, improving maintainability by 40% and reducing load times by 15%, delivering high-quality solutions.",
+    tech: ["React", "HTML", "CSS", "JavaScript"],
+  },
+  {
+    id: 1,
+    company: "Henry",
+    title: "FullStack Developer",
+    date: "Aug 2022 - Dec 2022",
+    description:
+      "Mentored students in web development with ReactJS and NodeJS, reducing dropout rates by 20% and empowering skills through hands-on projects and a supportive environment.",
+    tech: ["React", "NodeJs", "JavaScript", "MongoDB", "MySQL", "Redux"],
+  },
+];
