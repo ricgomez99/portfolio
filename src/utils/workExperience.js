65a0790311cbd7b301c40a53d7f9fc9e2a1bca38
Andres <argomez.restrepo@gmail.com>
2025-02-04 15:00:10 -0500

    Added exp timeline

--- src/utils/workExperience.js
+++ src/utils/workExperience.js
@@ -6,16 +6,6 @@ export const workExperience = [
     date: "May 2024 - Present",
     description:
       "Developed a scalable NFC-enabled web app with ReactJS, NestJS, and AWS, boosting productivity by 25% and improving API connectivity by 10% through backend engineering.",
-    tech: [
-      "React",
-      "NestJS",
-      "TypeScript",
-      "JavaScript",
-      "MongoDB",
-      "Mongoose",
-      "Kafka",
-      "TailwindCSS",
-    ],
   },
   {
     id: 2,
@@ -24,7 +14,6 @@ export const workExperience = [
     date: "Mar 2023 - Sep 2023",
     description:
       "Built and optimized 30+ web projects with ReactJS and modular CSS, improving maintainability by 40% and reducing load times by 15%, delivering high-quality solutions.",
-    tech: ["React", "HTML", "CSS", "JavaScript"],
   },
   {
     id: 1,
@@ -33,6 +22,5 @@ export const workExperience = [
     date: "Aug 2022 - Dec 2022",
     description:
       "Mentored students in web development with ReactJS and NodeJS, reducing dropout rates by 20% and empowering skills through hands-on projects and a supportive environment.",
-    tech: ["React", "NodeJs", "JavaScript", "MongoDB", "MySQL", "Redux"],
   },
 ];
