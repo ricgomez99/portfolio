e573b5fa8c8e9c507eb63f46776a030e287f24c2
Andres <argomez.restrepo@gmail.com>
2025-02-07 12:46:45 -0500

    added education section

---
+++ src/utils/education.js
@@ -0,0 +1,4 @@
+export const education = [
+  { title: "Soy Henry", subtitle: "FullStack Developer", date: "2022-2033" },
+  { title: "SENA", subtitle: "Mobile Developer", date: "2024-Present" },
+];
